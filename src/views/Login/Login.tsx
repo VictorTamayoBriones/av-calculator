9ab4c1707af845f8401de9be45e752017be17f24
VictorTamayoBriones <victortamayo509@gmail.com>
2023-06-09 00:41:17 -0600

    feat: update login

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,12 +1,19 @@
+import LOGINBG from '@/assets/images/login-bg.png'
+import { LoginContainer } from './login.styles'
 
 function Login() {
   return (
-    <form>
-      <h1>Grupo a vivir</h1>
-      <input type="text" placeholder="user"/>
-      <input type="text" placeholder="password"/>
-      <button>login</button>
-    </form>
+    <LoginContainer>
+      <div className="brand">
+        <img src={LOGINBG} alt="Grupo a vivir" />
+      </div>
+      <form>
+        <h1>Welcome again</h1>
+        <input type="text" placeholder="user"/>
+        <input type="text" placeholder="password"/>
+        <button>login</button>
+      </form>
+    </LoginContainer>
   )
 }
 
