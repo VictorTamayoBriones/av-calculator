636edc3a95cf724d97d77e231ab6ef8f323390d6
VictorTamayoBriones <victortamayo509@gmail.com>
2023-06-17 00:15:37 -0600

    feat: :sparkles: update button style

--- src/views/Home/components/BtnGetPdf/BtnGetPdf.tsx
+++ src/views/Home/components/BtnGetPdf/BtnGetPdf.tsx
@@ -33,13 +33,13 @@ function BtnGetPdf() {
         <>
             {/* <button onClick={()=>{console.log('first')}} disabled={name === '' || honorarios === '0' || total === '0'}  >Generar contrato</button> */}
             {
-                name === '' || honorarios === '0' || total === '0' ? ''
-                :
-                <PDFDownloadLink document={<ContratoPdf name={data.name} total_cantidad={data.total_cantidad} total_letra={data.total_letra} honorarios_cantidad={data.honorarios_cantidad} honorarios_letra={data.honorarios_letra} date={data.date} />} fileName="contrato.pdf">
-                    {
-                        ({loading}) => loading ? 'Loading' : 'Download'
-                    }     
-                </PDFDownloadLink>
+                <button disabled={name === '' || honorarios === '0' || total === '0'} >
+                    <PDFDownloadLink document={<ContratoPdf name={data.name} total_cantidad={data.total_cantidad} total_letra={data.total_letra} honorarios_cantidad={data.honorarios_cantidad} honorarios_letra={data.honorarios_letra} date={data.date} />} fileName="contrato.pdf">
+                        {
+                            ({loading}) => loading ? 'Loading' : 'Download'
+                        }     
+                    </PDFDownloadLink>
+                </button>
             }
         </>
     )
